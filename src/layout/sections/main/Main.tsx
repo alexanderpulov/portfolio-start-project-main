a188fe8d5a50d10cb8827cf058b8b2c679648e20
alexanderpulov <alec2918@gmail.com>
2023-08-18 10:21:30 +0300

    first commit

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import photo from "../../../assets/images/photo.webp";
+import styled from "styled-components";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+  return (
+    <StyledMain>
+      <FlexWrapper align="center" justify="space-around">
+        <Text>
+          <span>Hi There</span>
+          <span>I am Svetlana Dyablo</span>
+          <h1>A Web Developer.</h1>
+        </Text>
+
+        <Photo src={photo} alt="/" />
+      </FlexWrapper>
+    </StyledMain>
+  );
+};
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`;
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: antiquewhite;
+`;
+
+const Text = styled.div`
+  h1 {
+    color: red;
+  }
+`;
---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+import { Icon } from "../../../components/icon/Icon";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Skill } from "./skill/Skill";
+
+export const Skills = () => {
+  return (
+    <StyledSkills>
+      <SectionTitle>My Skills</SectionTitle>
+      <FlexWrapper wrap="wrap">
+        <Skill
+          iconId="code"
+          title="html5"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        />
+        <Skill
+          iconId="css"
+          title="css3"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim "
+        />
+        <Skill
+          iconId="react"
+          title="React"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        />
+        <Skill
+          iconId="typescript"
+          title="typescript"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        />
+        <Skill
+          iconId="styled"
+          title="styled components"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        />
+        <Skill
+          iconId="figma"
+          title="WEB DESIgN"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        />
+      </FlexWrapper>
+    </StyledSkills>
+  );
+};
+
+const StyledSkills = styled.section`
+  background-color: #ffd8d8;
+  min-height: 100vh;
+`;
