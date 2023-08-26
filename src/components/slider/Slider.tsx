2012a42d2c9a39475113b9d9199387bf7bfd074d
alexanderpulov <alec2918@gmail.com>
2023-08-26 16:44:57 +0300

    testimony css

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { FlexWrapper } from "../FlexWrapper";
+import { theme } from "../../styles/Theme";
 
 export const Slider = () => {
   return (
@@ -17,7 +18,7 @@ export const Slider = () => {
       </FlexWrapper>
       <Pagination>
         <span></span>
-        <span></span>
+        <span className="active"></span>
         <span></span>
       </Pagination>
     </StyledSlider>
@@ -38,15 +39,32 @@ const Slide = styled.div`
 
 const Text = styled.p``;
 
-const Name = styled.span``;
+const Name = styled.span`
+  text-align: center;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  margin: 22px 0 42px;
+  display: inline-block;
+`;
 
 const Pagination = styled.div`
   span {
     display: inline-block;
-    margin: 5px;
-    width: 10px;
-    height: 10px;
-    border-radius: 5px;
-    background-color: #ffae44;
+    width: 7px;
+    height: 7px;
+    border-radius: 20px;
+    background: rgba(255, 255, 255, 0.5);
+
+    & + span {
+      margin-left: 5px;
+    }
+
+    &.active {
+      background-color: ${theme.colors.accent};
+      width: 20px;
+    }
   }
 `;
