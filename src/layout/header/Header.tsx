5bdbd23e0e5cd80f2e2560bcf337deb18b2cb10e
alexanderpulov <alec2918@gmail.com>
2023-08-26 13:29:10 +0300

    main css

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
-import { Menu } from "../../components/menu/Menu";
 import { Container } from "../../components/Container";
 import { FlexWrapper } from "../../components/FlexWrapper";
 import { theme } from "../../styles/Theme";
+import { HeaderMenu } from "./headerMenu/HeaderMenu";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
 
@@ -14,7 +14,7 @@ export const Header = () => {
       <Container>
         <FlexWrapper justify="space-between" align="center">
           <Logo />
-          <Menu menuItems={items} />
+          <HeaderMenu menuItems={items} />
         </FlexWrapper>
       </Container>
     </StyledHeader>
@@ -22,5 +22,11 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-  background-color: ${theme.colors.primaryBg};
+  background-color: rgba(31, 31, 32, 0.9);
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
 `;
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,36 +2,92 @@ import React from "react";
 import photo from "../../../assets/images/photo.webp";
 import styled from "styled-components";
 import { FlexWrapper } from "../../../components/FlexWrapper";
+import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper align="center" justify="space-around">
-        <Text>
-          <span>Hi There</span>
-          <span>I am Svetlana Dyablo</span>
-          <h1>A Web Developer.</h1>
-        </Text>
+      <Container>
+        <FlexWrapper align="center" justify="space-between">
+          <div>
+            <SmallText>Hi There</SmallText>
+            <Name>
+              I am <span>Svetlana Dyablo</span>
+            </Name>
+            <MainTitle>A Web Developer.</MainTitle>
+          </div>
 
-        <Photo src={photo} alt="/" />
-      </FlexWrapper>
+          <PhotoWrapper>
+            <Photo src={photo} alt="/" />
+          </PhotoWrapper>
+        </FlexWrapper>
+      </Container>
     </StyledMain>
   );
 };
+
+const StyledMain = styled.section`
+  min-height: 100vh;
+  background-color: antiquewhite;
+  display: flex;
+`;
+
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: "";
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    z-index: -1;
+  }
+`;
 
 const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  display: flex;
 `;
 
-const StyledMain = styled.section`
-  min-height: 100vh;
-  background-color: antiquewhite;
+const SmallText = styled.span`
+  font-size: 14px;
+  font-weight: 400;
 `;
 
-const Text = styled.div`
-  h1 {
-    color: red;
+const Name = styled.h1`
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 50px;
+  font-weight: 700;
+  letter-spacing: 2.5px;
+  margin: 10px 0;
+
+  span {
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+      content: "";
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+    }
   }
+`;
+
+const MainTitle = styled.h2`
+  font-size: 27px;
+  font-weight: 400;
 `;
