2614229b430992d1158f9a1297037f0f06a64d54
alexanderpulov <alec2918@gmail.com>
2023-09-04 12:23:11 +0300

    refactoring header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,34 +1,34 @@
 import React from "react";
-import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Container } from "../../components/Container";
 import { FlexWrapper } from "../../components/FlexWrapper";
-import { theme } from "../../styles/Theme";
-import { HeaderMenu } from "./headerMenu/HeaderMenu";
-import { MobileMenu } from "./mobileMenu/MobileMenu";
+import { DesktopMenu } from "./headerMenu/desktopMenu/DesktopMenu";
+
+import { S } from "./Header_Styles";
+import { MobileMenu } from "./headerMenu/mobileMenu/MobileMenu";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
 
-export const Header = () => {
+export const Header: React.FC = () => {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 768;
+
+  React.useEffect(() => {
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+  }, []);
+
   return (
-    <StyledHeader>
+    <S.Header>
       <Container>
         <FlexWrapper justify="space-between" align="center">
           <Logo />
-          <HeaderMenu menuItems={items} />
-          <MobileMenu menuItems={items} />
+          {width < breakpoint ? (
+            <MobileMenu menuItems={items} />
+          ) : (
+            <DesktopMenu menuItems={items} />
+          )}
         </FlexWrapper>
       </Container>
-    </StyledHeader>
+    </S.Header>
   );
 };
-
-const StyledHeader = styled.header`
-  background-color: rgba(31, 31, 32, 0.9);
-  padding: 20px 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 999;
-`;
