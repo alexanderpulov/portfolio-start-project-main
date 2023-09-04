470425862db9f7dd4e04aa24ff7d7d22c859f474
alexanderpulov <alec2918@gmail.com>
2023-09-04 14:27:40 +0300

    refactoring testimony, contacts, slogan

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,61 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import { S } from "./Contacts_Styles";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Button } from "../../../components/Button";
 import { Container } from "../../../components/Container";
-import { theme } from "../../../styles/Theme";
 
 export const Contacts = () => {
   return (
-    <StyledContacts>
+    <S.Contacts>
       <Container>
         <SectionTitle>Contact</SectionTitle>
-        <StyledForm>
-          <Field placeholder="Name" />
-          <Field placeholder="Name" />
-          <Field placeholder="Name" as="textarea" />
+        <S.Form>
+          <S.Field placeholder="Name" />
+          <S.Field placeholder="Name" />
+          <S.Field placeholder="Name" as="textarea" />
           <Button type="submit">Send message</Button>
-        </StyledForm>
+        </S.Form>
       </Container>
-    </StyledContacts>
+    </S.Contacts>
   );
 };
-
-const StyledContacts = styled.section``;
-
-const StyledForm = styled.form`
-  max-width: 540px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 16px;
-  margin: 0 auto;
-
-  textarea {
-    resize: none;
-    height: 155px;
-  }
-`;
-
-const Field = styled.input`
-  width: 100%;
-  border: 1px solid ${theme.colors.borderColor};
-  background-color: ${theme.colors.secondaryBg};
-  padding: 7px 15px;
-
-  color: ${theme.colors.font};
-  font-family: Poppins, sans-serif;
-  font-size: 12px;
-  font-weight: 400;
-  letter-spacing: 0.6px;
-
-  &::placeholder {
-    color: ${theme.colors.placeholder};
-    text-transform: capitalize;
-  }
-
-  &:focus-visible {
-    outline: 1px solid ${theme.colors.borderColor};
-  }
-`;
--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,11 +4,10 @@ import { SectionTitle } from "../../../components/SectionTitle";
 import { Icon } from "../../../components/icon/Icon";
 import { Slider } from "../../../components/slider/Slider";
 import { FlexWrapper } from "../../../components/FlexWrapper";
-// import { IconWrapper } from "../skills/skill/Skill";
 import { S } from "../skills/Skills_Styles";
 import { Container } from "../../../components/Container";
 
-export const Testimony = () => {
+export const Testimony: React.FC = () => {
   return (
     <StyledTestimony>
       <Container>
